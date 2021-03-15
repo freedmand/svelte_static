af65d0529e8899951c2d3cdd487afe74bd84b44a
freedmand <freedmand@gmail.com>
2021-03-15 12:35:48 -0400

    Actually add scss support

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import css from 'rollup-plugin-css-only';
 import execute from 'rollup-plugin-execute'
+import { scss } from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -12,6 +13,7 @@ export default {
   },
   plugins: [
     svelte({
+      preprocess: [scss()],
       compilerOptions: {
         immutable: true,
         hydratable: true,
--- src/Index.svelte
+++ src/Index.svelte
@@ -1,10 +1,10 @@
 <svelte:head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>TODO</title>
-  <link rel='icon' type='image/png' href='/favicon.png'>
-  <meta name="description" content="TODO"/>
+  <link rel="icon" type="image/png" href="/favicon.png" />
+  <meta name="description" content="TODO" />
 </svelte:head>
 
 TODO: Replace this text and all other "TODO"s
